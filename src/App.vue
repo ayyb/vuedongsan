f35340e0043bbedee3fed88388e7b3ed40fb8c95
ayyb <ayyb@kakao.com>
2021-12-01 14:41:39 +0900

    데이터바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    원룸샵
+    <h4 class="red" :style="red">XX 원룸</h4>
+    <p>{{price1}}만원</p>
+  </div>
+  <div>
+    <h4>XX 원룸</h4>
+    <p>{{price2}}만원</p>
+  </div>
+  <ul v-for="(item,index) in products" :key="index">
+    <h4 :style="bold">{{item}}</h4>
+    <p>{{price2}}만원</p>
+  </ul>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return{
+      price1 : 600,
+      price2 : 700,
+      products: ['역삼동원룸','천호동원룸','마포구원룸'],
+      red: 'color:red',
+      bold: 'bold'
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
